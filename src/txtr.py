9d8dac579b1a9581a8c7d0642b6098c18c05db75
KOZ39 <koz39c@gmail.com>
2018-07-30 15:51:14 +0900

    Rename png

--- src/txtr.py
+++ src/txtr.py
@@ -4,27 +4,27 @@ import time
 DIR = 'TXTR'
 
 begin = time.time()
-idx = 0
 
 with open('data.win', mode='rb') as f:
     d = f.read()
 
+idx = 0 #d.find(b'TXTR')
+cnt = -1
+
 pathlib.Path(f'{DIR}').mkdir(parents=True, exist_ok=True)
 
-while(idx < len(d) - 16):
+while d[idx:idx+4] != b'AUDO':
     if d[idx:idx+16] == b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR': # PNG Header
         p = idx
         idx += 16
+        cnt += 1
 
         while d[idx:idx+8] != b'IEND\xaeB`\x82': # PNG Footer
             idx += 1
         idx += 8
 
-        with open(f'{DIR}/{p}.png', mode='wb') as f:
+        with open(f'{DIR}/{str(cnt).zfill(4)}.png', mode='wb') as f:
             f.write(d[p:idx])
-
-        if d[idx:idx+4] == b'AUDO':
-            break
     else:
         idx += 1
 
