1c79a20db854a589fe3023cd21f191370aa7b391
KOZ39 <koz39c@gmail.com>
2018-07-31 19:40:02 +0900

    Significant performance improvements

---
+++ src/png.py
@@ -0,0 +1,26 @@
+import pathlib
+import time
+
+DIR = 'png'
+
+begin = 0
+end = 0
+cnt = 0
+
+with open('data.win', mode='rb') as f:
+    d = f.read()
+
+pathlib.Path(f'{DIR}').mkdir(parents=True, exist_ok=True)
+
+while True:
+    begin = d.find(b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR', end + 1)
+
+    if begin == -1:
+        break
+
+    end = d.find(b'IEND\xaeB`\x82', begin + 1)
+
+    cnt += 1
+
+    with open(f'{DIR}/{str(cnt).zfill(4)}.png', mode='wb') as f:
+        f.write(d[begin:end])
---
+++ src/wav.py
@@ -0,0 +1,29 @@
+import pathlib
+import time
+
+DIR = 'wav'
+
+begin = 0
+end = 0
+cnt = 0
+
+with open('data.win', mode='rb') as f:
+    d = f.read()
+
+pathlib.Path(f'{DIR}').mkdir(parents=True, exist_ok=True)
+
+while True:
+    begin = d.find(b'WAVEfmt', end)
+
+    if begin == -1:
+        break
+
+    end = d.find(b'WAVEfmt', begin + 1)
+
+    cnt += 1
+    
+    v1 = begin - 8
+    v2 = None if end == -1 else end
+
+    with open(f'{DIR}/{str(cnt).zfill(4)}.wav', mode='wb') as f:
+        f.write(d[v1:v2])
